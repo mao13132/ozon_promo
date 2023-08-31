9f1033c4884f4eafc01f9ed1028b5c6590594ad7
mao13132 <mao13132@gmail.com>
2023-08-31 09:19:05 +0300

    start project

---
+++ settings.py
@@ -0,0 +1,13 @@
+NAME_API_FILE = 'ozonproject-1.json'  # Наименования файла-ключа от API google. Файл поместить в папку google_api_file
+
+ID_SHEET = '1Emwc-lwopwyBjNmtasvg8RemHmQM4iSKllQoUV9pM20'
+
+BLACK_NAME = []
+
+NAME_SHEET = ["Severli"]
+
+PHONE = '9648325336'  # Без 7 и 8
+
+NAME_PROFILE = 'promo'
+
+NAME_SERVER = 'My_comp'
